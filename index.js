d497da2a454c629c652fcb1bb649710e08a5091b
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-19 12:25:43 +0700

    deploy heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
         sameSite : none
     })
 )
